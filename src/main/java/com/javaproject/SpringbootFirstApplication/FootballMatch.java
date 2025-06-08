e6c749ab35e57d6f5d359897b5bb01eba458150f
AndreiCatalin <139058102+ZanfirAndreiCatalin@users.noreply.github.com>
2025-06-08 13:28:18 +0300

    Update FootballMatch.java

--- src/main/java/com/javaproject/SpringbootFirstApplication/FootballMatch.java
+++ src/main/java/com/javaproject/SpringbootFirstApplication/FootballMatch.java
@@ -4,55 +4,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FootballMatch {
+    private int id;
     private String homeTeam;
     private String awayTeam;
     private String date;
     private String time;
+    private List<Bet> bets = new ArrayList<>();
 
-    private final List<Bet> bets; // List of bets for this match
-    private int Id;
+    public FootballMatch() {}
 
-    public FootballMatch(int Id, String homeTeam, String awayTeam, String date, String time) {
-        this.Id = Id;
+    public FootballMatch(int id, String homeTeam, String awayTeam, String date, String time) {
+        this.id = id;
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.date = date;
-        this.time = time;
-        this.bets = new ArrayList<>();
-    }
-    public String getHomeTeam(){
-        return homeTeam;
-    }
-    public void setHomeTeam(String homeTeam){
-        this.homeTeam = homeTeam;
-    }
-
-    public String getAwayTeam(){
-        return awayTeam;
-    }
-    public void  setAwayTeam(String awayTeam){
-        this.awayTeam = awayTeam;
-    }
-
-    public String getDate(){
-        return date;
-    }
-    public void setDate(String date){
-        this.date = date;
-    }
-
-    public String getTime(){
-        return time;
-    }
-    public void setTime(String time) {
         this.time = time;
     }
 
     public int getId() {
-        return Id;
+        return id;
     }
-    public void setId(int Id) {
-        this.Id = Id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public List<Bet> getBets() {
